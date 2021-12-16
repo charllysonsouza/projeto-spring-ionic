1586557ea6c9e08881dc15fda81742302b3076e3
Charllyson Souza <charllyson.souza@hotmail.com>
2021-12-16 10:25:31 -0300

    testando a primeira classe de domínio - Categoria

---
+++ src/main/java/com/cursomc/models/Categoria.java
@@ -0,0 +1,49 @@
+package com.cursomc.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+}
