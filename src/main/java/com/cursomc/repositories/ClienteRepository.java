c1ba9e2ab3e62d81d0274c5b72f6456bc18f4d03
Charllyson Souza <charllyson.souza@hotmail.com>
2022-01-22 17:31:04 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/cursomc/repositories/ClienteRepository.java
@@ -1,9 +1,12 @@
 package com.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cursomc.models.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
+	
+	@Transactional(readOnly = true)
+	public boolean existsByEmail(String email);
 }
--- src/main/java/com/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,18 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.cursomc.controllers.exception.FieldMessage;
 import com.cursomc.dto.ClienteNewDTO;
 import com.cursomc.models.enums.TipoCliente;
+import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -19,6 +25,7 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
 		List<FieldMessage> list = new ArrayList<>();
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
@@ -29,6 +36,9 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 		
+		if(clienteRepository.existsByEmail(objDto.getEmail())) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
