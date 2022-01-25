cfca9dab9a4ec8f6104fd13a780c3b7531a3b16c
Charllyson Souza <charllyson.souza@hotmail.com>
2022-01-25 20:55:48 -0300

    Criando profile de teste

--- src/main/java/com/cursomc/controllers/PedidoController.java
+++ src/main/java/com/cursomc/controllers/PedidoController.java
@@ -2,8 +2,6 @@ package com.cursomc.controllers;
 
 import java.net.URI;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
