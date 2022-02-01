46cdee101c7987553c13e7791a5dc4e1614379b7
Charllyson Souza <charllyson.souza@hotmail.com>
2022-01-31 22:37:52 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/cursomc/config/JacksonConfig.java
+++ src/main/java/com/cursomc/config/JacksonConfig.java
@@ -3,6 +3,8 @@ package com.cursomc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.cursomc.models.PagamentoComBoleto;
 import com.cursomc.models.PagamentoComCartao;
@@ -21,5 +23,10 @@ public class JacksonConfig {
 			}
 		};
 		return builder;
+	}
+	
+	@Bean
+	public JavaMailSender jMS (){
+		return new JavaMailSenderImpl();
 	}
 }
